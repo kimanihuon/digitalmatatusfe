9b80594af6729af13419b0cbdf4eb696a540e1a9
kimani <kimanihube@gmail.com>
2020-05-18 00:40:39 +0300

    added initial build

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-content>
+      <Nav />
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Nav from "./components/nav";
+
+export default {
+  components: {
+    Nav
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/nav.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-app-bar app clipped-left dense color="deep-purple" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list nav dense>
+        <v-list-item-group active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false
+  })
+};
+</script>
